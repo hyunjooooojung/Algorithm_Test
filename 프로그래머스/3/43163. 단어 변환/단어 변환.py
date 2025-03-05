9d2744003dc85b2094f25ba75735aa52865db533
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-03-05 19:14:38 +0900

    [level 3] Title: 단어 변환, Time: 7.21 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def check_word(word, check_word):
+    # 한 글자만 다른 단어인지 확인
+    count = 0
+    for i in range(len(word)):
+        if word[i] != check_word[i]:
+            count += 1
+    
+    if count == 1:
+        return True
+    else:
+        return False
+    
+def solution(begin, target, words): # BFS : 최소 단계 구하기
+    
+    if target not in words:
+        return 0
+    
+    queue = deque([(begin, 0)])  # (현재 단어, 변환 횟수)
+    
+    while queue:
+        now_word, count = queue.popleft()
+        
+        if now_word == target:
+            return count
+        
+        for word in words:
+            if check_word(now_word, word):
+                queue.append((word, count+1))
+                
+    return answer
