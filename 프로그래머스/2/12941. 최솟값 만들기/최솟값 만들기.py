1c72fba4e4d3aacd27c3bdc53f10acb73493fbb0
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-03-05 18:09:01 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.44 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.py
@@ -7,3 +7,6 @@ def solution(A,B):
         answer += A[i] * B[i]
 
     return answer
+    
+    # 한줄로 끝내는 코드네... -> zip 함수 사용
+    # return sum([a * b for a, b in zip(sorted(A), sorted(B, reverse=True))])
