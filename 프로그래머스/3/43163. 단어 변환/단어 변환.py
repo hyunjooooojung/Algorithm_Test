0c15609960bfa8fc1e68850a86d2b6a5155982ef
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-03-05 19:21:29 +0900

    [level 3] Title: 단어 변환, Time: 0.59 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -2,15 +2,19 @@ from collections import deque
 
 def check_word(word, check_word):
     # 한 글자만 다른 단어인지 확인
-    count = 0
-    for i in range(len(word)):
-        if word[i] != check_word[i]:
-            count += 1
+#     count = 0
+#     for i in range(len(word)):
+#         if word[i] != check_word[i]:
+#             count += 1
     
-    if count == 1:
-        return True
-    else:
-        return False
+#     if count == 1:
+#         return True
+#     else:
+#         return False
+    
+    # (ADD) 한줄로 표현했을 때
+    count = sum([word[i] != check_word[i] for i in range(len(word))])
+    return count == 1
     
 def solution(begin, target, words): # BFS : 최소 단계 구하기
     
@@ -18,7 +22,8 @@ def solution(begin, target, words): # BFS : 최소 단계 구하기
         return 0
     
     queue = deque([(begin, 0)])  # (현재 단어, 변환 횟수)
-    visited = set() # 방문한 단어 체크
+    visited = set() # (ADD) 방문한 단어 체크 -> 중복 방문으로 인한 비효율성 방지 위해 필요함!
+    
     while queue:
         now_word, count = queue.popleft()
         
@@ -27,7 +32,6 @@ def solution(begin, target, words): # BFS : 최소 단계 구하기
         
         for word in words:
             if word not in visited and check_word(now_word, word):
-                visited.add(word)  # 방문 체크
+                visited.add(word)  # (ADD) 방문 체크
                 queue.append((word, count+1))
                 
-    return answer
