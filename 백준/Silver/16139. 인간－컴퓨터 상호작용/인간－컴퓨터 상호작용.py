cf05bb85dcda86f747022829d8efdd860d2281c7
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-03-07 17:11:22 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 656 ms, Memory: 94412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,15 +1,17 @@
+# https://developer-project.tistory.com/418 님의 풀이를 참고함.
+
 import sys
 input = sys.stdin.readline
 
-S = input() # 문자열
+S = input().rstrip() # 문자열
 q = int(input()) # 질문의 수
+
+count = [[0] * 26]
+for i in range(len(S)):
+    count.append(count[len(count) - 1][:])
+    count[i+1][ord(S[i]) - 97] += 1
 
 for _ in range(q):
     a, l, r = input().split()
-
-    count = 0
-    for s in S[int(l):int(r)+1]:
-        if a == s:
-            count += 1
-    
-    print(count)
+    result = count[int(r) + 1][ord(a) - 97] - count[int(l)][ord(a) - 97]
+    print(result)
