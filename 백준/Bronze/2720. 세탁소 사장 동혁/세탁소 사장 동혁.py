9fbd5e8ffd7d9fd102bce92644c4afdfe9b12cff
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-03-21 14:36:34 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -3,19 +3,7 @@ input = sys.stdin.readline
 
 T = int(input())
 for _ in range(T):
-    C = int(input()) # 거스름돈(센트)
-    change = C
-    a, b, c, d = 0, 0, 0, 0 # 쿼터, 다임, 니켈, 페니
-    while change:
-        a += change // 25
-        change -= (a*25)
-        b += change // 10
-        change -= (b*10)
-        c += change // 5
-        change -= (c*5)
-        d += change // 1
-        change -= (d*1)
-
-        if change == 0:
-            print(a, b, c, d)
-            break
+    C = int(input())
+    for i in [25, 10, 5, 1]:
+        print(C // i, end=" ")
+        C = C % i
