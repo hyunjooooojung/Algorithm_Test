01e6dfc2294b9f2ab9ffeea9d2715f1384fad857
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-04-05 00:58:09 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.11 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -2,11 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int n) {
-        String n_3 = Integer.toString(n, 3);
-        String reverse = "";
-        for (int i=n_3.length()-1; i>=0; i--) {
-            reverse += n_3.charAt(i);
-        }
+        // String n_3 = Integer.toString(n, 3);
+        // String reverse = "";
+        // for (int i=n_3.length()-1; i>=0; i--) {
+        //     reverse += n_3.charAt(i);
+        // }
+        // int answer = Integer.parseInt(reverse, 3);
+        
+        String three = Integer.toString(n, 3);
+        StringBuffer sb = new StringBuffer(three);
+        String reverse = sb.reverse().toString();
+        
         int answer = Integer.parseInt(reverse, 3);
         
         return answer;
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
 
 ### 성능 요약
 
-메모리: 99.6 MB, 시간: 12.85 ms
+메모리: 72.8 MB, 시간: 0.11 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 05일 00:46:41
+2025년 04월 05일 00:58:06
 
 ### 문제 설명
 
