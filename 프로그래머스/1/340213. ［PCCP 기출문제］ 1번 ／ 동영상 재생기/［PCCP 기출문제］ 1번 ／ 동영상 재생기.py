190c70e57076250fa24b37e2c3857101db071f3a
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-04-05 18:37:04 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 0.25 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.py
@@ -1,4 +1,5 @@
 def solution(video_len, pos, op_start, op_end, commands):
+    
     def minute_to_second(string):
         mm, ss = map(int, string.split(":"))
         return mm * 60 + ss
@@ -6,21 +7,24 @@ def solution(video_len, pos, op_start, op_end, commands):
     def second_to_minute(time):
         mm = str(time // 60).zfill(2)
         ss = str(time % 60).zfill(2)
-        
         return f"{mm}:{ss}"
+    
+    def opening_skip(pos):
+        if minute_to_second(op_start) <= pos <= minute_to_second(op_end):
+            return minute_to_second(op_end)
+        else:
+            return pos
         
     pos = minute_to_second(pos)
-        
+    
     for command in commands:
-        if minute_to_second(op_start) <= pos <= minute_to_second(op_end):
-            pos = minute_to_second(op_end)
+        pos = opening_skip(pos)
             
         if command == "prev":
             pos = max(0, pos-10)
         if command == "next":
             pos = min(minute_to_second(video_len), pos+10)
         
-        if minute_to_second(op_start) <= pos <= minute_to_second(op_end):
-            pos = minute_to_second(op_end)
+        pos = opening_skip(pos)
     
     return second_to_minute(pos)
