f21c1e0e7f70af809f9eafc2592db35cebc9db66
JUNG HYUNJOO <113044908+hyunjooooojung@users.noreply.github.com>
2025-04-13 21:16:31 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 616 ms, Memory: 67064 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,25 +1,32 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
 
-def dfs(graph, v, visited):
-    visited[v] = True
-    for i in graph[v]:
-        if visited[i] == False:
-            dfs(graph, i, visited)
+from collections import deque
+
+def bfs(v, graph, visited):
+    visited[v] = 1
+    queue = deque([v])
+
+    while queue:
+        x = queue.popleft()
+
+        for i in graph[x]:
+            if visited[i] == 0:
+                queue.append(i)
+                visited[i] = 1
 
 N, M = map(int, input().split())
-graph = [[] for _ in range(N+1)]
-visited = [False for _ in range(N+1)]
+graph = [[] * (N+1) for _ in range(N+1)]
+visited = [0] * (N+1)
+
 for _ in range(M):
     u, v = map(int, input().split())
-    graph[u].append(v) # 무방향 그래프
+    graph[u].append(v)
     graph[v].append(u)
 
 count = 0
 for i in range(1, N+1):
-    if visited[i] == False:
+    if visited[i] == 0:
+        bfs(i, graph, visited)
         count += 1
-        dfs(graph, i, visited) 
-
 print(count)
